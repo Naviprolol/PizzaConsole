0e6ba9e49f24cd59d40bd865d4bd32b0e14cc2e7
PsevdoKot <youkos00@mail.ru>
2023-07-07 11:17:54 +0300

    Расширен роутинг, добавлены новые компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { OrdersPageComponent } from './components/orders-page/orders-page.component';
+import { ProductsPageComponent } from './components/products-page/products-page.component';
+import { StaffPageComponent } from './components/staff-page/staff-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    OrdersPageComponent,
+    ProductsPageComponent,
+    StaffPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
