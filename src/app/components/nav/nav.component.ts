69fd385191515dcabfe64449753cd0ce13c5a0a0
PsevdoKot <youkos00@mail.ru>
2023-07-10 23:29:47 +0300

    Добавил навигационный сайдбар

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnDestroy} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {filter, Subscription} from "rxjs";
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+}
